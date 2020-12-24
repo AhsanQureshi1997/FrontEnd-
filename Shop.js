2052a95aa3ac608b3e25373c50203caeb6061bf9
AhsanQureshi1997 <64383283+AhsanQureshi1997@users.noreply.github.com>
2020-12-23 23:49:20 -0600

    Add files via upload

---
+++ Shop.js
@@ -0,0 +1,16 @@
+// const square = function(number){
+
+//     return number*number;
+// }
+
+
+class Person(){
+    constructor(name){
+        this.name = name;
+    }
+    walk(){
+        console.log('walk');
+    }
+}
+
+const person = new Person('Ahsan');
